7c8ac8bccb996ed963e9ae2e58f9fd20f5c44d88
Clay Ford <joe.clayton.ford@gmail.com>
2021-07-20 16:27:35 -0400

    format lme app code

--- inst/shiny-apps/lme/app.R
+++ inst/shiny-apps/lme/app.R
@@ -10,40 +10,50 @@ ui <- fluidPage(
 
    sidebarLayout(
       sidebarPanel(
-        numericInput(inputId = "N",label = "Number of subjects", value = 5, min = 3, max = 10, step = 1),
-        numericInput(inputId = "n",label = "Number of obs. per subject", value = 10, min = 3, max = 15, step = 1),
+        numericInput(inputId = "N",label = "Number of subjects",
+                     value = 5, min = 3, max = 10, step = 1),
+        numericInput(inputId = "n",label = "Number of obs. per subject",
+                     value = 10, min = 3, max = 15, step = 1),
         radioButtons(inputId = "type","Model Type:",
                      choices = c("Random intercept" = "ri",
                                  "Random slope" = "rs",
                                  "Random intecept and random slope" = "ris")),
 
         h3("Fixed Effects"),
-        sliderInput("a","Intercept coefficient:",min = -5,max = 5,value = 0,step = 0.1),
-        sliderInput("b","Slope coefficient:",min = -5,max = 5,value = 0,step = 0.1),
+        sliderInput("a","Intercept coefficient:",
+                    min = -5,max = 5,value = 0,step = 0.1),
+        sliderInput("b","Slope coefficient:",
+                    min = -5,max = 5,value = 0,step = 0.1),
 
         h3("Random Effects"),
         conditionalPanel(
           condition = "input.type == 'ri' | input.type == 'ris'",
-          sliderInput("d1", "Intercept Std.Dev.", min = 0.1, max = 5, value = 0.1, step = 0.1)
+          sliderInput("d1", "Intercept Std.Dev.",
+                      min = 0.1, max = 5, value = 0.1, step = 0.1)
         ),
         conditionalPanel(
           condition = "input.type == 'rs' | input.type == 'ris'",
-          sliderInput("d2", "Slope Std.Dev.", min = 0.1, max = 5, value = 0.1, step = 0.1)
+          sliderInput("d2", "Slope Std.Dev.",
+                      min = 0.1, max = 5, value = 0.1, step = 0.1)
         ),
         conditionalPanel(
           condition = "input.type == 'ris'",
-          sliderInput("cor", "Intercept and Slope correlation", min = -1, max = 1, value = 0, step = 0.1)
+          sliderInput("cor", "Intercept and Slope correlation",
+                      min = -1, max = 1, value = 0, step = 0.1)
         ),
-        sliderInput("r","Residuals:", min = 0.01, max = 5, value = 0.01, step = 0.1),
+        sliderInput("r","Residuals:",
+                    min = 0.01, max = 5, value = 0.01, step = 0.1),
         actionButton(inputId = "gen", "Generate new data")
 
       ),
 
       # Show a plot of the fitted lmer model
       mainPanel(
-        p('Set the parameters of the data generating process using the controls in the left panel.
-          See how well', code('lmer'), 'estimates the parameters in the model output and accompanying graph. The thick
-          black line is the fitted fixed effect. The colored lines represent the subject-specific fitted lines.'),
+        p('Set the parameters of the data generating process using the controls
+        in the left panel. See how well', code('lmer'), 'estimates the
+        parameters in the model output and accompanying graph. The thick
+        black line is the fitted fixed effect. The colored lines represent the
+        subject-specific fitted lines.'),
         plotOutput("lmePlot"),
         verbatimTextOutput("summary")
       )
@@ -72,9 +82,11 @@ server <- function(input, output) {
            ris = list(Z <- X,
                       d12 <- cor*d1*d2,
                       u <- rmvnorm(n = 1, mean = c(0,0),
-                                   sigma = matrix(c(d1^2,d12,d12,d2^2), nrow = 2)))
+                                   sigma = matrix(c(d1^2,d12,
+                                                    d12,d2^2),
+                                                  nrow = 2)))
     )
-    e <- rnorm(n,mean = 0, sd = r)
+    e <- rnorm(n, mean = 0, sd = r)
     Y <- X %*% B + Z %*% t(u) + e
     data.frame(y = Y, x = X[,-1])
   }
@@ -82,9 +94,13 @@ server <- function(input, output) {
    output$lmePlot <- renderPlot({
      input$gen
      dat <- bind_rows(replicate(input$N,
-                                expr = Yi(n = input$n, a = input$a, b = input$b,
-                                          d1 = input$d1, d2 = input$d2, cor = input$cor,
-                                          r = input$r, type = input$type), simplify = F),
+                                expr = Yi(n = input$n,
+                                          a = input$a, b = input$b,
+                                          d1 = input$d1, d2 = input$d2,
+                                          cor = input$cor,
+                                          r = input$r,
+                                          type = input$type),
+                                simplify = F),
                       .id = "id")
      fo <- switch(input$type,
             ri = y ~ x + (1|id),
@@ -101,8 +117,11 @@ server <- function(input, output) {
                         id = factor(unique(dat$id)))
      ggplot(dat, aes(x = x, y = y, color = id)) +
        geom_point() +
-       geom_abline(slope = fixef(mod)[2], intercept = fixef(mod)[1], size = 2) +
-       geom_abline(mapping = aes(slope = b, intercept = a, color = id), data = dat2)
+       geom_abline(slope = fixef(mod)[2],
+                   intercept = fixef(mod)[1], size = 2) +
+       geom_abline(mapping = aes(slope = b, intercept = a, color = id),
+                   data = dat2) +
+       ylim(c(-40,40))
    })
 }
 
